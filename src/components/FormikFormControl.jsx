b488d56af460b8bb1513c8330a80e46324241fc8
Animes <animesbarman101@gmail.com>
2025-07-14 17:02:08 +0600

    yup

--- src/components/FormikFormControl.jsx
+++ src/components/FormikFormControl.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
  import { useFormik } from 'formik';
+ import * as Yup from 'yup';
+ import { string, number} from 'yup';
 
 const FormikFormControl = () => {
 
@@ -12,6 +14,19 @@ const FormikFormControl = () => {
        email: '',
        password:'',
      },
+     validationSchema: Yup.object({
+      name: Yup.string().min(4).required(),
+      email:Yup.string().email(),
+      password:Yup.string()
+    .required('Password is required')
+    .min(5, 'Password must be at least 5 characters')
+    .max(10, 'Password must be at most 10 characters')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,10}$/,
+      'Password must contain letters and numbers only'
+    ),
+
+}),
      onSubmit: (values,{resetForm})=> {
        console.log(values)
 
@@ -25,8 +40,11 @@ const FormikFormControl = () => {
     <div>
         <form onClick={formik.handleSubmit} action="">
             <input onChange={formik.handleChange} value={formik.values.name} type="text" name="name" id="name" />
+            <p>{formik.errors.name}</p>
             <input onChange={formik.handleChange} value={formik.values.email} type="email" name="email" id="email" />
+            <p>{formik.errors.email}</p>
             <input onChange={formik.handleChange} value={formik.values.password} type="password" name="password" id="password" />
+            <p>{formik.errors.password}</p>
             <button type="submit">Submit</button>
         </form>
     </div>
